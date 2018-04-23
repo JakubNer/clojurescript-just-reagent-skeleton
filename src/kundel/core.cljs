021353b8b4a184aa18f21b1ff76c181182107a24
Jakub Ner <jakub_ner@hotmail.com>
2018-04-23 19:09:19 -0400

    Initial submit

---
+++ src/kundel/core.cljs
@@ -0,0 +1,12 @@
+(ns min-reagent.core
+  (:require
+    [reagent.core :as r]))
+
+(defn my-app []
+  [:div
+    [:h1 "Hello Reagent"]
+    [:p "ClojureScript + React + Atoms + Hiccup"]])
+
+(r/render
+  [my-app]
+  (js/document.getElementById "appdiv"))
